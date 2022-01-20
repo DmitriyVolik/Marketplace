5b12d9be4ae804c7f33c1e4cff672826bdd15d1e
DmitriyVolik <60630678+DmitriyVolik@users.noreply.github.com>
2022-01-20 22:39:56 +0200

    Added controllers and views for auth

--- Marketplace/Models/User.cs
+++ Marketplace/Models/User.cs
@@ -5,5 +5,9 @@ namespace Marketplace.Models
         public int Id { get; set; }
         
         public string Username { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string PasswordHash { get; set; }
     }
 }

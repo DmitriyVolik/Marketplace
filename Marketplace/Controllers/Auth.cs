a86c3202380527afb019f48267b98dd36720f8b4
unknown <advisior1@gmail.com>
2022-01-20 14:52:49 +0200

    signinstart

---
+++ Marketplace/Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}

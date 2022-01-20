472c1ed73add026f5ad2b50181b1faf5ce034a47
DmitriyVolik <60630678+DmitriyVolik@users.noreply.github.com>
2022-01-20 15:17:13 +0200

    fix run bug(deleted database)

--- Marketplace/Startup.cs
+++ Marketplace/Startup.cs
@@ -6,7 +6,6 @@ using Marketplace.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,10 +25,6 @@ namespace Marketplace
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<Context>(opts => {
-                opts.UseSqlServer(
-                    Configuration["ConnectionStrings:Database"]);
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

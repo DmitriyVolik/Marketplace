b94ca8cd5f98bd44d7518c96b9931cd809f7fb34
unknown <advisior1@gmail.com>
2022-01-26 22:24:12 +0200

    updts

--- Marketplace/Controllers/Posts.cs
+++ Marketplace/Controllers/Posts.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Marketplace.Models;
+using Marketplace.Models.DB;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,18 +15,39 @@ namespace Marketplace.Controllers
 {
     public class Posts : Controller
     {
+        IWebHostEnvironment _appEnvironment;
+        Context _db;
+
+        public Posts(Context _db, IWebHostEnvironment appEnvironment)
+        {
+            this._db = _db;
+            _appEnvironment = appEnvironment;
+        }
+
         [Authorize]
         public IActionResult AddPost()
         {
-            return View();
+            return View(new PostEditViewModel() 
+            { 
+                Categories = _db.Categories
+            });
         }
 
         [HttpPost]
         [Authorize]
-        public IActionResult CreatePost(PostEditViewModel model, IFormFileCollection uploads )
+        public async Task<IActionResult> CreatePost(PostEditViewModel model, IFormFileCollection uploads )
         {
-            Console.WriteLine(model.Post.Title);
-            Console.WriteLine(uploads.Count);
+            if (uploads.Count != 0)
+            {
+                foreach (var item in uploads)
+                {
+                    using (var writer = new FileStream($"{_appEnvironment.WebRootPath}/images/{Guid.NewGuid()}_{item.FileName}", FileMode.Create))
+                    {
+                        await item.CopyToAsync(writer);
+                        Console.WriteLine($"{item.FileName}: сохранён.");
+                    }
+                }
+            }
             return View("AddPost");
         }
     }
--- Marketplace/Models/PostEditViewModel.cs
+++ Marketplace/Models/PostEditViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using Marketplace.Models.DB;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Marketplace.Models
 {
     public class PostEditViewModel
     {
         public Post Post { get; set; }
+        public IQueryable<Category> Categories { get; set; }
     }
 }

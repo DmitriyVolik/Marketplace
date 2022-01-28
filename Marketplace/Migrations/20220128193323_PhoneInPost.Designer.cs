ddf01f4e75679e08c01a272c362c0ff31570ca69
DmitriyVolik <60630678+DmitriyVolik@users.noreply.github.com>
2022-01-28 21:34:23 +0200

    fix migrations

--- Marketplace/Migrations/20220128193323_PhoneInPost.Designer.cs
+++ Marketplace/Migrations/20220128193323_PhoneInPost.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Marketplace.Models.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Marketplace.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220128193323_PhoneInPost")]
+    partial class PhoneInPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Marketplace/Migrations/20220128193323_PhoneInPost.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marketplace.Migrations
+{
+    public partial class PhoneInPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Posts");
+        }
+    }
+}
